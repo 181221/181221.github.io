8a9c27823cedbabf187134e218aa4aa7ef84f7ae
Peder Wiig Aalen <peaa@computas.com>
2023-09-26 23:37:49 +0200

    mer info

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WeddingHeader from "./components/WeddingHeader.vue";
+import ImageTextBlock from "./components/ImageTextBlock.vue";
+import WeddingInfo from "./components/WeddingInfo.vue";
+import ImageBlock from "./components/ImageBlock.vue";
+import WeddingFooter from "./components/WeddingFooter.vue";
+</script>
 
 <template>
-  <div class="main-container">
-    <h1 class="main-title">Peder <span class="main-title-span">&</span> Cecilie</h1>
-    <h2 class="main-subtitle">
-      <span class="main-subtitle-date">13. July 2024 </span><span class="main-dot" /> <span>Kjeller Gaard</span>
-    </h2>
-    <h3 class="main-subtitle">Hold av datoen, mer informasjon om bryllupet kommer</h3>
-  </div>
+  <WeddingHeader />
+  <ImageTextBlock imageUrl="/otto.jpg" />
+  <WeddingInfo />
+  <ImageBlock imageUrl="/otto.jpg" />
+  <WeddingFooter />
 </template>
 
-<style scoped>
-.main-container {
-  color: white;
-}
-.main-title {
-  margin: 12px 0;
-}
-.main-dot {
-  height: 12px;
-  width: 12px;
-  background-color: white;
-  border-radius: 50%;
-  display: inline-block;
-  vertical-align: middle;
-}
-.main-title-span {
-  display: block;
-}
-
-.main-subtitle-date {
-  display: block;
-  margin-bottom: 0.1em;
-}
-.main-dot {
-  display: none;
-}
-@media (min-width: 600px) {
-  .main-title-span {
-    display: inline;
-  }
-
-  .main-subtitle-date {
-    display: inline;
-  }
-  .main-dot {
-    display: inline-block;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/WeddingFooter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="footer">
+    <div class="footer-section">
+      <h2 class="footer-title">Kontakt</h2>
+      <p>Har du spørsmål eller ønsker å komme i kontakt med oss?</p>
+      <a href="mailto:bryllup@pederogcecilie.no">Send oss en e-post</a>
+    </div>
+    <div class="footer-section">
+      <h2 class="footer-title">Innspill og taler</h2>
+      <p>Har du ideer, innspill eller ønsker å holde en tale under bryllupet?</p>
+      <p>
+        Kontakt våre toastmastere: <a href="mailto:bryllup@pederogcecilie.no">Sigurd</a> eller
+        <a href="mailto:bryllup@pederogcecilie.no">Jeanette</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  margin: 2em 0;
+}
+a {
+  color: black;
+}
+</style>
