770d6c6b9d8b4f179a1368bba7bb1cd0e9eb6ccf
Peder Wiig <peder.wiig@skyetec.no>
2024-01-14 14:05:46 +0100

    Oppdater mailliste

--- src/components/WeddingFooter.vue
+++ src/components/WeddingFooter.vue
@@ -10,10 +10,7 @@
     <div class="footer-section">
       <h2 class="footer-title">Innspill og taler</h2>
       <p>Har du ideer, innspill eller ønsker å holde en tale under bryllupet?</p>
-      <p>
-        Kontakt våre toastmastere: <a href="mailto:bryllup@pederogcecilie.no">Sigurd</a> eller
-        <a href="mailto:bryllup@pederogcecilie.no">Jeanette</a>
-      </p>
+      <p>Kontakt våre toastmastere: <a href="toastmaster@pederogcecilie.no">Sigurd og Jeanette</a></p>
     </div>
   </div>
 </template>
